518d724705b09abe3b10ad74d0bc96ad5060c135
Elisa Boscani <elisabosc90@gmail.com>
2023-09-11 15:06:41 +0200

    inserted image

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, 
-presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data. */
+presa anch’essa inserendo il percoso in una propietá dell'oggetto
+ restituito dalla funzione data. */
 
 const { createApp } = Vue;
 
@@ -10,6 +11,7 @@ createApp({
   data() {
     return {
       text: "Primo esercizio con veu.js",
+      img: "https://picsum.photos/id/237/200/300",
     };
   },
 }).mount("#app");
